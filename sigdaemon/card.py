90174431f57f487e57cd3f3f1694abcef82c3575
Kim Tore Jensen <kim@incendio.no>
2015-03-26 19:03:44 +0000

    More

--- sigdaemon/card.py
+++ sigdaemon/card.py
@@ -8,19 +8,22 @@ import subprocess
 import mpd
 
 SOCK = "tcp://localhost:9090"
-mode = 'neutral'
-mpd_client = mpd.MPDClient()
 
-# filename='/var/log/card.log', filemode='a', 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
+button_mode = 'neutral'
+button_mode = 'mode'
 
-# Socket to talk to server
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 logging.info("Setting up ZeroMQ socket...")
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 socket.connect(SOCK)
 socket.setsockopt_string(zmq.SUBSCRIBE, u'')
 logging.info("Listening for events from signal daemon on %s" % SOCK)
+mpd_client = mpd.MPDClient()
+
+#
+# Actual implementations
+#
 
 def run_dmc(cmd):
     logging.info("Running command: %s" % ' '.join(cmd))
@@ -30,7 +33,12 @@ def run_dmc(cmd):
     return exit_code, stderr, stdout
 
 def shutdown():
-    run_dmc(['init', '0'])
+    logging.info("Shutting down the entire system!")
+    run_dmc(['/sbin/init', '0'])
+
+def android_toggle_screen():
+    logging.info("Toggling Android screen on/off")
+    run_dmc(['/usr/local/bin/adb', 'shell', 'input', 'keyevent', '26'])
 
 def mpd_connect():
     mpd_client.timeout = 5
@@ -45,38 +53,74 @@ def mpd_get_status():
         status = mpd_client.status()
     return status
 
-def play_or_pause():
+def mpd_prev():
+    status = mpd_get_status()
+    logging.info("Switching to previous song")
+    mpd_client.previous()
+
+def mpd_next():
+    status = mpd_get_status()
+    logging.info("Switching to next song")
+    mpd_client.next()
+
+def mpd_play_or_pause():
     status = mpd_get_status()
     if status['state'] == 'stop':
+        logging.info("Starting to play music")
         mpd_client.play()
     else:
+        logging.info("Toggling play/pause state")
         mpd_client.pause()
 
-def volume_shift(increment):
+def mpd_volume_shift(increment):
     status = mpd_get_status()
     shifted = int(status['volume']) + increment
     if shifted > 100:
         shifted = 100
     if shifted < 0:
         shifted = 0
+    logging.info("Setting volume to %d" % shifted)
     mpd_client.setvol(shifted)
 
+#
+# Dispatcher functions
+#
+
 def dispatch_neutral_rotary_left(params):
-    volume_shift(-3)
+    mpd_volume_shift(-3)
+
+def dispatch_mode_rotary_left(params):
+    mpd_prev()
 
 def dispatch_neutral_rotary_right(params):
-    volume_shift(3)
+    mpd_volume_shift(3)
+
+def dispatch_mode_rotary_right(params):
+    mpd_next()
 
 def dispatch_neutral_rotary_press(params):
-    play_or_pause()
+    mpd_play_or_pause()
+
+def dispatch_mode_rotary_press(params):
+    android_toggle_screen()
 
 def dispatch_neutral_power_off(params):
     shutdown()
 
+def dispatch_mode_power_off(params):
+    """
+    Shutting down the power when the MODE key is held down prevents the
+    hardware from being turned off at all.
+    """
+    logging.info("MODE key held down when turning off power, system is NOT shutting down.")
+
 def get_dispatch_function(tokens):
+    """
+    Figure out which dispatcher function to use
+    """
     pattern = "dispatch_%s_%s"
     base_name = '_'.join(tokens).lower()
-    func_name = pattern % (mode, base_name)
+    func_name = pattern % (button_mode, base_name)
     logging.debug("Looking for function name '%s'" % func_name)
     try:
         func = globals()[func_name]
@@ -84,6 +128,10 @@ def get_dispatch_function(tokens):
         return None
     return func
 
+
+#
+# Main loop
+#
 
 if __name__ == '__main__':
     try:
@@ -94,7 +142,7 @@ if __name__ == '__main__':
             func = get_dispatch_function(tokens)
             try:
                 if callable(func):
-                    logging.info("Dispatching to '%s'" % str(func.__name__))
+                    logging.debug("Dispatching to '%s'" % str(func.__name__))
                     func([])
                 else:
                     logging.warn("No dispatcher found for this event")
