ceadda88119bda729ec4bac15bdde302c773529b
Kim Tore Jensen <kim@incendio.no>
2015-06-19 11:49:47 +0000

    Android screen toggle replaced by ADB again

--- sigdaemon/card.py
+++ sigdaemon/card.py
@@ -52,7 +52,8 @@ def try_shutdown():
 
 def android_toggle_screen():
     logging.info("Toggling Android screen on/off")
-    run_dmc(['curl', 'http://cardroid:8765'])
+    #run_dmc(['curl', 'http://cardroid:8765'])
+    run_dmc(['/usr/local/bin/adb', 'shell', 'input', 'keyevent', '26'])
 
 def mpd_connect():
     mpd_client.timeout = 5
