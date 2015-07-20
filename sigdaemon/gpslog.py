16d337e44ac2c58436729b82bfd125506d5fc2d7
Kim Tore Jensen <kim@incendio.no>
2015-07-20 23:41:22 +0200

    Add gpslog.py, logging GPS coordinates to syslog every minute

---
+++ sigdaemon/gpslog.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python2.7
+#
+# Log coordinates to syslog every N seconds
+
+import gps
+import syslog
+import datetime
+
+
+LOG_INTERVAL = 60
+
+
+def add_attr(report, line, key):
+    if hasattr(report, key):
+        line += [(key, report[key])]
+
+def make_report_line(report):
+    output = []
+    [add_attr(report, output, key) for key in ['time', 'lat', 'lon', 'alt', 'speed']]
+    line = ' '.join(['%s=%s' % (k, v) for k, v in output])
+    line = "%dD fix: %s" % (report['mode'], line)
+    return line
+
+if __name__ == '__main__':
+    syslog.openlog('gpslog', syslog.LOG_PID, syslog.LOG_DAEMON)
+    session = gps.gps()
+    session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+    report = {}
+    then = datetime.datetime.fromtimestamp(0)
+
+    try:
+        for r in session:
+            if r['class'] != 'TPV':
+                continue
+            report = r
+            now = datetime.datetime.now()
+            delta = now - then
+            if delta.total_seconds() > LOG_INTERVAL:
+                then = now
+                syslog.syslog(make_report_line(report))
+    except Exception, e:
+        syslog.syslog("Uncaught exception, terminating!")
+        syslog.syslog(unicode(e))
