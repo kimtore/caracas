0827e557fe785276620e565ab917fa6a07cfc30e
Kim Tore Jensen <kim@incendio.no>
2015-12-19 01:54:52 +0100

    Document gpscat

--- sigdaemon/gpscat.py
+++ sigdaemon/gpscat.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python2.7
 #
-# Output a file with NMEA sentences, sleeping one second between each chunk
-#
 
 import time
 import socket
 import argparse
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('file', action='store')
-    parser.add_argument('bind', action='store')
+    description = """
+    Stream a file with recorded NMEA sentences on a TCP server suitable for
+    gpsd, sleeping N seconds between each update.
+    """
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--sleep', type=float, default=1, required=False, help='How many seconds between each update')
+    parser.add_argument('file', action='store', help='File with NMEA sentences')
+    parser.add_argument('bind', action='store', help='TCP socket to listen on, e.g. localhost:3333')
     args = parser.parse_args()
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,7 +30,7 @@ def main():
                 if len(line) == 0:
                     break
                 if line[:6] == '$GPGGA':
-                    time.sleep(1)
+                    time.sleep(args.sleep)
                 connection.sendall(line)
                 print line,
         connection.close()
