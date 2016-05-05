d4ecb227fb29b821826372eb50761caeb4301a93
Kim Tore Jensen <kim@incendio.no>
2016-05-05 21:12:19 +0000

    Allow fading from value to value in backlightctl

--- utils/backlightctl.py
+++ utils/backlightctl.py
@@ -11,24 +11,48 @@ CONNECT_TIMEOUT = 0.05
 socket = None
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('value', type=int)
-    args = parser.parse_args()
-
-    # client sanitation, other values will be ignored on the receiver
-    if args.value < 0:
-        args.value = 0
-    elif args.value > 100:
-        args.value = 100
-
+def get_zmq_socket():
     context = zmq.Context()
     socket = context.socket(zmq.PUB)
     socket.connect(SOCK)
-
     # wait a bit before sending the messages
     time.sleep(CONNECT_TIMEOUT)
+    return socket
 
-    socket.send_string("BACKLIGHT %d" % args.value)
-    print 'Setting backlight intensity to %d%%' % args.value
-    socket.close()
+
+def set_value(a):
+    socket = get_zmq_socket()
+    print 'Setting backlight intensity to %d%%' % a.value
+    socket.send_string("BACKLIGHT %d" % a.value)
+
+
+def fade(a):
+    socket = get_zmq_socket()
+    print 'Fading backlight intensity from %d%% to %d%%' % (a.start, a.end)
+    if a.start == a.end:
+        return
+    direction = 1 if a.start < a.end else -1
+    x = a.start
+    while True:
+        x += direction
+        socket.send_string("BACKLIGHT %d" % x)
+        print "BACKLIGHT %d" % x
+        if x == a.end:
+            break
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    parser_set = subparsers.add_parser('set')
+    parser_set.add_argument('value', type=int)
+    parser_set.set_defaults(func=set_value)
+
+    parser_fade = subparsers.add_parser('fade')
+    parser_fade.add_argument('start', type=int)
+    parser_fade.add_argument('end', type=int)
+    parser_fade.set_defaults(func=fade)
+
+    args = parser.parse_args()
+    args.func(args)
